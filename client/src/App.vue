0429ccb99a87ca12899a5528de196faaf6ece12b
bekzod818 <bekzod030900@gmail.com>
2022-01-03 01:53:53 +0500

    front

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mt-5">
+    <Container />
+  </div>
+</template>
+
+<script>
+import Container from './components/Container.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Container
+  }
+}
+</script>
+
+<style>
+</style>
